1c7f0419dc9ad62956cc41956bb018433e544c67
Patrick <923079848@qq.com>
2019-07-29 00:19:57 +0800

    遍历链接（广度优先搜索）

---
+++ ChapterFour/findlinks.go
@@ -0,0 +1,30 @@
+package ChapterFour
+
+import (
+	"fmt"
+	"log"
+)
+
+//广度优先搜索
+func BreadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func Crawl(url string) []string {
+	fmt.Println(url)
+	list, err := Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
